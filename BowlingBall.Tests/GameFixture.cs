f8fc2af2fbf95976e05467f399f58f6f48326320
annadarade <daradeanna@gmail.com>
2021-11-14 16:52:36 +0530

    Tavisca Bowling Ball Game code submit

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,251 @@
+﻿// Author : Anna Darade.   
+// Created Test Cases for BowlingBall Game
+// Created on : 14 Nov 2021
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        private Game _game=null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _game = new Game();
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_StrikesAndSpare()
+        {
+            //Arrange
+            _game.Roll(10);
+            _game.Roll(9);
+            _game.Roll(1);
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(7);
+            _game.Roll(2);
+            _game.Roll(10);
+            _game.Roll(10);
+            _game.Roll(10);
+            _game.Roll(9);
+            _game.Roll(0);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(9);
+            _game.Roll(1);
+            _game.Roll(10);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(187, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_NoExtraRoll()
+        {
+            //Arrange
+            _game.Roll(4); 
+            _game.Roll(3);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(7);
+            _game.Roll(5);
+            _game.Roll(2);
+            _game.Roll(5);
+            _game.Roll(3);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(10);
+            _game.Roll(9);
+            _game.Roll(0);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(134, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_StrikeThenSpareAtEnd()
+        {
+            //Arrange
+            _game.Roll(4);
+            _game.Roll(2);
+            _game.Roll(7);
+            _game.Roll(3);
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(7);
+            _game.Roll(5);
+            _game.Roll(2);
+            _game.Roll(5);
+            _game.Roll(3);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(10);
+            _game.Roll(10);
+            _game.Roll(9);
+            _game.Roll(1);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(154, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_SpareThenStrikeAtEnd()
+        {
+            //Arrange
+            _game.Roll(4);
+            _game.Roll(2);
+            _game.Roll(7);
+            _game.Roll(3);
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(7);
+            _game.Roll(5);
+            _game.Roll(2);
+            _game.Roll(5);
+            _game.Roll(3);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(10);
+            _game.Roll(9);
+            _game.Roll(1);
+            _game.Roll(10);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(145, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_RandomGame_And_ThreeStrikesAtEnd()
+        {
+            //Arrange
+            _game.Roll(5);
+            _game.Roll(2);
+            _game.Roll(7);
+            _game.Roll(3);
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(7);
+            _game.Roll(5);
+            _game.Roll(2);
+            _game.Roll(5);
+            _game.Roll(3);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(8);
+            _game.Roll(2);
+            _game.Roll(10);
+            _game.Roll(10);
+            _game.Roll(10);
+            _game.Roll(10);
+           
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(166, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_RollBall_With_AllZero()
+        {
+            //Arrange
+            RollBall(0, 20);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(0, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_RollBall_With_AllOne()
+        {
+            //Arrange
+            RollBall(1, 20);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(20, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_PerfectGame()
+        {
+            //Arrange
+            RollBall(10, 12);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(300, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_RollBall_With_OneSpare()
+        {
+            //Arrange
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+            RollBall(1, 17);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(33, actualResult);
+        }
+
+        [TestMethod]
+        public void Test_Roll_With_OneStrike()
+        {
+            //Arrange
+            _game.Roll(10);
+            _game.Roll(3);
+            _game.Roll(4);
+             RollBall(0, 16);
+
+            //Act
+            int actualResult = _game.GetScore();
+
+            //Assert
+            Assert.AreEqual(24, actualResult);
+        }
+        
+        private void RollBall(int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ BowlingBallScoring/Game.cs
@@ -0,0 +1,80 @@
+﻿// Author : Anna Darade.   
+// Created BowlingBall Game for Tavisca
+// Created on : 14 Nov 2021
+
+// Solution Details :
+//1. The game consists of 10 frames,In each frame the player has two opportunities to knock
+//   down 10 pins.
+//2. The GetScore method gives total socre which is calculated by the total number of pins knocked down,
+//   plus bonuses for strikes and spares.
+//3. A spare is when the player knocks down all 10 pins in two tries. The bonus for that frame is the number of pins
+//   knocked down by the next roll.
+//4. A strike is when the player knocks down all 10 pins on his first try. The bonus for that frame is the value of the
+//   next two balls rolled.
+//5. Assumtions : The solution should assume valid inputs at all times and does not need to include unnecessary validations
+//   trying to prevent bad input data.
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        private int[] _rolls = new int[21];
+        private int[] _frame = new int[10];
+        private int _currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }     
+
+        public int GetScore()
+        {
+            int score = 0;
+            int index = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (VerifyStrike(index))
+                {
+                    score += CalculateStrikeScore(index);
+                    index++;
+                }
+                else if (VerifySpare(index))
+                {
+                    score += CalculateSpareScore(index);
+                    index += 2;
+                }
+                else
+                {
+                    score += CalculateStandardScore(index);
+                    index += 2;
+                }
+            }
+            return score;
+        }
+
+        private bool VerifyStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private bool VerifySpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+        }
+
+        private int CalculateStrikeScore(int index)
+        {
+            return 10 + _rolls[index + 1] + _rolls[index + 2];           
+        }
+
+        private int CalculateSpareScore(int index)
+        {
+            return 10 + _rolls[index + 2];
+        }
+
+        private int CalculateStandardScore(int index)
+        {
+            return _rolls[index] + _rolls[index + 1];
+        }
+    }
+}
